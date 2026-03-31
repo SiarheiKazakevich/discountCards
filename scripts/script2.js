0245460ca78ebd5092514ade44d4cfe7ea455ecb
Siarhei Kazakevich <dianrom@tut.by>
2026-03-31 22:02:45 +0300

    feat: fix color of header

--- scripts/script2.js
+++ scripts/script2.js
@@ -3,9 +3,9 @@ function toggleMenu() {
   const menu = document.getElementById('menu');
   const burger = document.getElementById('burger');
   const overlay = document.getElementById('overlay');
-  const header = document.querySelector('header')
+  //const header = document.querySelector('header')
   menu.classList.toggle('active');
   burger.classList.toggle('active');
   overlay.classList.toggle('active');
-  header.classList.toggle('active');
+  //header.classList.toggle('active');
 }
